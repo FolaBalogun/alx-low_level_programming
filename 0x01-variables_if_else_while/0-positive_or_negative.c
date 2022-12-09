7eae8b3a0a10814cd5acc869f6cea7a067e98bdc
FolaBalogun <abimbola.adenike1@gmail.com>
2022-12-09 14:08:58 -0800

    Pos or Neg

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stlib.h>
+#include <time.h>
+#include <stdio.h>
+/* comment */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* code */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
