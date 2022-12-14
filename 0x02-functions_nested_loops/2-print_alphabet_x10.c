8d1863c898191325eaa5baf16dd4ea172f62d60a
FolaBalogun <abimbola.adenike1@gmail.com>
2022-12-13 17:45:08 -0800

    times ten

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10.c - print alphabet 10 times
- * Return: Always 0
+	 * print_alphabet_x10.c - print alpha 10 times
  */
-
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
 	int ten;
 	char la;
@@ -14,8 +11,9 @@ void print_alphabet_x10(void);
 	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
+		{
 			_putchar(la);
+		}
 		_putchar('\n');
-		return (0);
 	{
 }
