6ad1fbde2503fa73fc8ab71c8683f1bd5f1cb5ca
Fola <117811752+FolaBalogun@users.noreply.github.com>
2022-12-15 22:06:18 +0100

    Largest Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,23 +9,24 @@
  */
 int largest_number(int a, int b, int c)
 {
-        int largest;
+int largest;
 
-  printf("Enter three different numbers: ");
-  scanf("%lf %lf %lf", &a, &b, &c);
-
-  // if a is greater than both b and c, a is the largest
-  if (a >= b && b >= c)
-    printf("%.2f is the largest number.", a);
-
-  // if b is greater than both a and c, a is the largest
-  if (b >= a && b >= c)
-    printf("%.2f is the largest number.", b);
-
-  // if c is greater than both a and b, c is the largest
-  if (c >= a && c >= b)
-    printf("%.2f is the largest number.", c);
-
-  return largest;
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+largest =b;
+}
+else
+{
+largest = c;
 }
 
+return (largest);
+}
