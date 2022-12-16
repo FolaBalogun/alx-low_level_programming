c0dca7892fa6d1e3b1ce0e081079ee858e687fab
FolaBalogun <abimbola.adenike1@gmail.com>
2022-12-15 18:45:36 -0800

    print nums

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,19 +2,14 @@
 
 /**
  * print_numbers - print numbers
- * Return: Always 0
+ * Return: 0 - 9 followed by a new line
  */
 
 void print_numbers(void)
 {
 int i;
 
-i = 0;
-while (i < 10)
-{
+for (i = 0; i <= 9; i++)
 _putchar (i + '0');
-i++;
-}
-
 _putchar ('\n');
 }
