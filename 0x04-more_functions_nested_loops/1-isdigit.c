4768325617e2891a8b474476b833bcb99e9cd564
Fola <117811752+FolaBalogun@users.noreply.github.com>
2022-12-16 01:57:04 +0100

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
-#include main.h
+#include "main.h"
+#include <stdio.h>
 
 /**
  * _isdigit - check if numbers are 0 - 9
@@ -8,8 +9,8 @@
 
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
-return (1);
-else
-return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
