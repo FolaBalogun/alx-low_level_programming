4a9631c73ac057511ef86759d6c266dccbf95464
FolaBalogun <abimbola.adenike1@gmail.com>
2022-12-21 06:23:44 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to be printed
+ * Return: 1
+ * On error, return -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
