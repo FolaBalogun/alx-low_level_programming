5a5f793c73034e920782075c727b8dbef6e6629c
FolaBalogun <abimbola.adenike1@gmail.com>
2022-12-22 15:01:36 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,6 @@
  * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
-
 {
 	int i;
 	int j;
@@ -21,7 +20,7 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] - src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
