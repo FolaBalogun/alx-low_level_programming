ad5a4d1d629315e98e0d0d23a278e942d54f7972
FolaBalogun <abimbola.adenike1@gmail.com>
2023-01-02 05:55:39 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -20,11 +20,11 @@ char *_strpbrk(char *s, char *accept)
 			{
 				break;
 			}
-			if (*(accept + j) != '\0')
-			{
-				return (s + i);
-			}
 		}
-		return (0);
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
 	}
+		return (0);
 }
