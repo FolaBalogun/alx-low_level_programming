01453baa1d12cedcd548c825f5925774f4a69cf2
FolaBalogun <abimbola.adenike1@gmail.com>
2023-01-04 17:27:14 -0800

    dream bigger

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,3 +1,16 @@
 #include "main.h"
 
 /**
+ * factorial -> prints the factorial of a given number
+ * @n: number
+ * Return: an integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
